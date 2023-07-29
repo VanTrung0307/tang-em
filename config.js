a4ff72f63fc729053e60fbd4dc396773edee7a34
Trung Văn <74370274+VanTrung0307@users.noreply.github.com>
2023-07-29 13:47:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐 Helluuuuuuu ! 🖐🖐',
-    introDesc: `Trung có điều này muốn hỏi Lam, Lam nhớ phải trả lời thật lòng nha`,
+    introDesc: `Anh có điều này muốn hỏi bé, bé nhớ phải trả lời thật lòng nha`,
     btnIntro: 'OK',
-    title: ' Lam thích Trung phải hông ? 😙',
-    desc: 'Lam hông trả lời hoặc thoát ra, tức là Lam thích Trung đó nha :> ',
-    btnYes: 'Có, Lam thật sự thích Trung 💖💖💖',
-    btnNo: '💔 Trung mơ à ! 💔 ',
-    question:'Nói cho Trung biết đi vì sao Lam thích Trung 😊😊',
+    title: ' Bé thích anh phải hông ? 😙',
+    desc: 'Bé hông trả lời hoặc thoát ra, tức là Bé thích Anh đó nha :> ',
+    btnYes: 'Có, Bé thật sự thích Anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Bé thích Anh 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì Trung đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Trung biết mà, Trung cũng thích Lam lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Trung nha. 💬💬',
+    reply: 'Tại vì Anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích Bé lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
     btnAccept: 'Ok lun 💖',
     messLink: 'https://m.me/vantrun0307'
 }
